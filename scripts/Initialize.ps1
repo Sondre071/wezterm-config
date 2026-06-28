dfae9f63873d363bcae2437a0b251bb12d31d0da
Sondre071 <sirciller@gmail.com>
2026-06-28 20:55:22 +0200

    Add initialization script

---
+++ scripts/Initialize.ps1
@@ -0,0 +1,24 @@
+$weztermConfigPath = "C:/Users/$env:USERNAME/.config/wezterm"
+$weztermUserDataPath =  "C:/Users/$env:USERNAME/AppData/Local/wezterm"
+$scriptNames = @('FavoriteCurrentDirectory.ps1', 'UnfavoriteCurrentDirectory.ps1')
+
+foreach ($name in $scriptNames)
+{
+    $sourcePath = Join-Path $weztermConfigPath 'scripts' $name
+    $symlinkPath = Join-Path $weztermUserDataPath 'scripts' 'Wezterm' $name
+    New-Item -ItemType SymbolicLink -Path $symlinkPath -Target $sourcePath -Force
+}
+
+$favoritePathsPath = Join-Path $weztermUserDataPath 'favorite_paths'
+
+if (-not (Test-Path $favoritePathsPath))
+{
+    New-Item -ItemType File -Path $favoritePathsPath
+}
+
+$backgroundPath = Join-Path $weztermUserDataPath 'background'
+
+if (-not (Test-Path $backgroundPath))
+{
+    New-Item -ItemType File -Path $backgroundPath
+}
