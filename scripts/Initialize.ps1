4122f28fb66beb6b26087f86d628e89c109ecd39
sondre071 <155721341+Sondre071@users.noreply.github.com>
2026-08-13 23:22:41 +0200

    quickfix: amend ps1 initialize script to work on windows

--- scripts/Initialize.ps1
+++ scripts/Initialize.ps1
@@ -1,40 +1,33 @@
 $ErrorActionPreference = 'Continue'
 
-$configPath = "$env:USERPROFILE/.config/wezterm"
-$userDataPath = "$env:USERPROFILE/.config/powershell"
+$userDataDir = "$env:USERPROFILE/.config/terminal"
 
-$scriptNames = @('FavoriteCurrentDirectory.ps1', 'UnfavoriteCurrentDirectory.ps1')
+$scriptNames = @('FavoriteCurrentDirectory.nu', 'UnfavoriteCurrentDirectory.nu')
 
-if (-not (Test-Path $userDataPath))
+if (-not (Test-Path $userDataDir))
 {
-    New-Item -ItemType Directory -Path $userDataPath
+    New-Item -ItemType Directory -Path $userDataDir
 }
 
 foreach ($name in $scriptNames)
 {
-    $sourcePath = Join-Path $configPath 'scripts', $name
-    $symlinkPath = Join-Path $userDataPath 'scripts', 'WezTerm', $name
+    $sourcePath = Join-Path $PSScriptRoot $name
+    $symlinkPath = Join-Path $userDataDir 'scripts', 'Terminal', $name
 
     New-Item -ItemType SymbolicLink -Path $symlinkPath -Target $sourcePath -Force
 }
 
-$favoritePathsPath = Join-Path $userDataPath 'favorite_paths'
+$favoritePathsPath = Join-Path $userDataDir 'favorite_paths'
 
 if (-not (Test-Path $favoritePathsPath))
 {
     New-Item -ItemType File -Path $favoritePathsPath
 }
 
-$backgroundConfigPath = Join-Path $userDataPath 'background'
+$backgroundConfigPath = Join-Path $userDataDir 'background.json'
 
 if (-not (Test-Path $backgroundConfigPath))
 {
     New-Item -ItemType File -Path $backgroundConfigPath
 }
 
-$backgroundsPath = Join-Path $userDataPath 'backgrounds'
-
-if (-not (Test-Path $backgroundsPath))
-{
-    New-Item -ItemType Directory -Path $backgroundsPath
-}
